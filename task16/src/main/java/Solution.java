cbd00e211bf356917d246a2b98ec6a1f77d8b127
Sergiy Kalyna <seryjk452@gmail.com>
2022-08-25 22:27:38 +0300

    Task16 Solved.

--- task16/src/main/java/Solution.java
+++ task16/src/main/java/Solution.java
@@ -8,31 +8,51 @@ public class Solution {
     public static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-
-
         int quantity = Integer.parseInt(READER.readLine());
         checkInputNumber(quantity);
 
+        List<String> cases = getListTestCases(quantity);
+
+        cases.forEach(testCase -> printCases(testCase));
+
+        READER.close();
     }
 
     public static void checkInputNumber(int number) {
         if (number < 0) {
             throw new RuntimeException("Input number is not valid");
-
         }
     }
 
-    public List<String> getListTestCases(int quantity) throws IOException {
+    public static List<String> getListTestCases(int quantity) throws IOException {
         List<String> inputList = new ArrayList<>();
 
-        for(int i =0; i<quantity; i++){
+        for (int i = 0; i < quantity; i++) {
             inputList.add(READER.readLine());
         }
 
         return inputList;
     }
 
-    public static void getCastType(String number){
+    public static void printCases(String number) {
+        try {
+            long res = Long.parseLong(number);
 
+            System.out.println(res + " can be fitted in:");
+            if (res >= -128 && res <= 127) {
+                System.out.println("* byte");
+            }
+            if (res >= Short.MIN_VALUE && res <= Short.MAX_VALUE) {
+                System.out.println("* short");
+            }
+            if (res >= Integer.MIN_VALUE && res <= Integer.MAX_VALUE) {
+                System.out.println("* int");
+            }
+            if (res >= Long.MIN_VALUE && res <= Long.MAX_VALUE) {
+                System.out.println("* long");
+            }
+        } catch (Exception e) {
+            System.out.println(number + " can't be fitted anywhere.");
+        }
     }
 }
