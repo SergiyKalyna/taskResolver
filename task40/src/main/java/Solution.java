3aa766fe297a8f08b5eacebad82601f60705a8f8
Sergiy Kalyna <seryjk452@gmail.com>
2022-10-14 17:39:55 +0300

    Task40 Resolved task.

---
+++ task40/src/main/java/Solution.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x/y);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        }
+
+    }
+}
